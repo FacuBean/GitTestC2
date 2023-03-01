7beb56754d9187f188fa4f39b507b12e36f782f1
Facundo Bustos Bean <facu.bustos.bean@hotmail.com>
2023-03-01 15:05:01 -0300

    prueba

---
+++ src/main/java/com/mycompany/clase/gittest/Clase2Gittest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase.gittest;
+
+/**
+ *
+ * @author facub
+ */
+public class Clase2Gittest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
